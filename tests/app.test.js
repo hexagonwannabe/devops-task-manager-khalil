8c94196a0b398411788f211aafa8f548557705e8
Khalil El bezzar <khalil.elcr7@gmail.com>
2026-04-07 01:54:00 +0100

    lab 5 start

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,5 +2,5 @@ const request = require('supertest');
 const app = require('../src/app');
 test('Health check', async () => {
 const res = await request(app).get('/');
- expect(res.statusCode).toBe(200);
+expect(res.statusCode).toBe(200);
 }); 
